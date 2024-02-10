53cde9e81456d26550ca2188a6261644c9dd6937
Pasero Christian <pasero.christian99@gmail.com>
2024-02-10 20:57:38 +0100

    added thesis

---
+++ thesis/build.sh
@@ -0,0 +1,3 @@
+cd ~/Desktop/thesis/
+pdflatex -halt-on-error main.tex
+evince main.pdf -i $1
---
+++ thesis/clean.sh
@@ -0,0 +1,8 @@
+rm thesis/main-blx.bib
+rm thesis/main-example.bib
+rm thesis/main.aux
+rm thesis/main.lof
+rm thesis/main.log
+rm thesis/main.lot
+rm thesis/main.run.xml
+rm thesis/main.toc
